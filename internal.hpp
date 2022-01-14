0b32be14b0c2b484af3b257dcfa8bc1b3d6d7d2a
おのおん <hika2420@gmail.com>
2022-01-14 10:53:19 +0900

    Add files via upload

---
+++ internal.hpp
@@ -0,0 +1,198 @@
+#pragma once
+
+#include "library_functions.hpp"
+#include "enumerations.hpp"
+#include "definitions.hpp"
+#include "pointer_utilties.hpp"
+
+static inline void stringWriteInternal(unsigned int *address,
+									   const unsigned char *string,
+									   unsigned long length) {
+	unsigned int stringIndex = 0;
+	enum dataType dataType = EIGHT_BIT;
+	unsigned int incrementPerIteration = 1;
+
+	for (; stringIndex < length; stringIndex += incrementPerIteration) {
+		writeInternal(address, string[stringIndex], dataType);
+		incrementPointerByAbsolute(&address, incrementPerIteration);
+	}
+}
+
+static inline void skipWriteInternal(unsigned int *address, unsigned int writesCount, unsigned int offsetBetweenWrites,
+									 unsigned int valueIncrement, unsigned int value, enum dataType dataType) {
+	for (unsigned int writesIndex = 0; writesIndex < writesCount; writesIndex++) {
+		writeInternal(address, value, dataType);
+
+		value += valueIncrement;
+		incrementPointerByAbsolute(&address, offsetBetweenWrites);
+	}
+}
+
+static inline void fillMemoryInternal(unsigned int *address, unsigned int length,
+									  unsigned int value, enum dataType dataType) {
+	unsigned int bytesCount = getBytesCount(dataType);
+	unsigned int writesCount = length / bytesCount;
+
+	for (unsigned int offset = 0; offset < writesCount; offset++) {
+		writeInternal(address, value, dataType);
+		incrementPointerByAbsolute(&address, bytesCount);
+	}
+}
+
+static inline void corrupterInternal(unsigned int *startingAddress, const unsigned int *endingAddress,
+									 unsigned int searchValue, unsigned int searchValueReplacement,
+									 enum dataType dataType) {
+	unsigned int bytesCount = getBytesCount(dataType);
+	unsigned int *currentAddress = startingAddress;
+
+	while (currentAddress < endingAddress) {
+		unsigned int currentValue = readInternal(currentAddress, dataType);
+		if (currentValue == searchValue) {
+			writeInternal(currentAddress, searchValueReplacement, dataType);
+		}
+
+		incrementPointerByAbsolute(&currentAddress, bytesCount);
+	}
+}
+
+static inline void
+writeSearchTemplateInternal(const unsigned char *searchTemplate,
+							unsigned int searchTemplateArraySize,
+							unsigned int matchCount,
+							unsigned int offset,
+							const unsigned char *replacement,
+							unsigned int replacementArraySize,
+							unsigned int *startingAddress,
+							const unsigned int *endingAddress) {
+	unsigned int countedMatches = 0;
+	unsigned int *currentAddress = startingAddress;
+
+	while (currentAddress < endingAddress) {
+		int comparisonResult = compareMemory(currentAddress, searchTemplate, searchTemplateArraySize);
+
+		if (comparisonResult == 0) {
+			countedMatches++;
+
+			if (countedMatches == matchCount) {
+				incrementPointerByAbsolute(&currentAddress, offset);
+				copyMemory(replacement, currentAddress, replacementArraySize);
+
+				return;
+			}
+		}
+
+		incrementPointerByAbsolute(&currentAddress, searchTemplateArraySize);
+	}
+}
+
+static inline void writePointerInternal(unsigned int *baseAddress, const unsigned int *startingMemoryRanges,
+										const unsigned int *endingMemoryRanges, const int *offsets,
+										unsigned int value, enum dataType dataType) {
+	unsigned int *currentAddress = baseAddress;
+	unsigned int *addressValue = 0;
+	unsigned int offsetsValue = 0;
+	int offsetsCount = getArraySize(offsets);
+
+	for (int offsetsIndex = 0; offsetsIndex < offsetsCount; offsetsIndex++) //入力されたoffestの文だけ繰り返す
+	{
+		// Read pointer value and add the offset
+		unsigned int *baseAddressValue = (unsigned int *) *currentAddress;//新しい変数にbaseAddressの値を代入
+		addressValue = baseAddressValue;
+		offsetsValue = offsets[offsetsIndex];
+
+		// Validate memory ranges
+		if (baseAddressValue < (unsigned int *) startingMemoryRanges[offsetsIndex] ||
+			baseAddressValue > (unsigned int *) endingMemoryRanges[offsetsIndex]) {
+			goto POINTER_FOLLOWING_FAILED;
+		}
+
+		currentAddress = addressValue + offsetsValue;
+	}
+
+	writeInternal(currentAddress, value, dataType);
+	writeInternal(0x14000090, currentAddress, dataType);
+
+	POINTER_FOLLOWING_FAILED:;
+}
+
+//複数のポインタを同時出力
+static inline void writeSomePointerInternal(unsigned int *baseAddress, const unsigned int *startingMemoryRanges,
+										const unsigned int *endingMemoryRanges, const int *offsets,
+										unsigned int value[], enum dataType dataType) {
+	unsigned int *currentAddress = baseAddress;
+	unsigned int *addressValue = 0;
+	unsigned int offsetsValue = 0;
+	int offsetsCount = getArraySize(offsets);
+
+	for (int offsetsIndex = 0; offsetsIndex < offsetsCount; offsetsIndex++) //入力されたoffestの文だけ繰り返す
+	{
+		// Read pointer value and add the offset
+		unsigned int *baseAddressValue = (unsigned int *) *currentAddress;//新しい変数にbaseAddressの値を代入
+		addressValue = baseAddressValue;
+		offsetsValue = offsets[offsetsIndex];
+		unsigned int writeValue = 0;
+
+		// Validate memory ranges
+		if (baseAddressValue < (unsigned int *) startingMemoryRanges[offsetsIndex] ||
+			baseAddressValue > (unsigned int *) endingMemoryRanges[offsetsIndex]) {
+			goto POINTER_FOLLOWING_FAILED;
+		}
+
+		currentAddress = addressValue + offsetsValue;
+		writeValue = value[offsetsIndex];
+		writeInternal(currentAddress, writeValue, dataType);
+		addressValue = 0;
+	}
+
+	POINTER_FOLLOWING_FAILED:;
+}
+
+static inline unsigned int readPointerInternal(unsigned int *baseAddress, const unsigned int *startingMemoryRanges,
+										const unsigned int *endingMemoryRanges, const int *offsets,
+										enum dataType dataType) {
+	unsigned int *currentAddress = baseAddress;
+	unsigned int *addressValue = 0;
+	unsigned int offsetsValue = 0;
+	int offsetsCount = getArraySize(offsets);
+
+	for (int offsetsIndex = 0; offsetsIndex < offsetsCount; offsetsIndex++) {
+		// Read pointer value and add the offset
+		unsigned int *baseAddressValue = (unsigned int *) *currentAddress;
+		addressValue = baseAddressValue;
+		offsetsValue = offsets[offsetsIndex];
+		
+		// Validate memory ranges
+		if (baseAddressValue < (unsigned int *) startingMemoryRanges[offsetsIndex] ||
+			baseAddressValue > (unsigned int *) endingMemoryRanges[offsetsIndex]) {
+			goto POINTER_FOLLOWING_FAILED;
+		}
+	
+		currentAddress = addressValue + offsetsValue;
+	}
+
+	return readInternal(currentAddress, dataType);
+
+	POINTER_FOLLOWING_FAILED:;
+}
+
+static inline unsigned int readSomePointerInternal(unsigned int *baseAddress, const unsigned int *startingMemoryRanges,
+										const unsigned int *endingMemoryRanges, const int *offsets,
+										enum dataType dataType) {
+	
+	unsigned int addresses[] = {};
+	int offsetsCount = getArraySize(offsets);
+
+	for(int i = 0; i < offsetsCount; i++)
+	{
+		addresses[i] = readPointerInternal(baseAddress, startingMemoryRanges, endingMemoryRanges, offsets[i], dataType);
+	}
+
+	return addresses[];
+}
+
+#define EXECUTE_ASSEMBLY_RETURN_ADDRESS 0x010F6AE0
+
+// This must be the last piece of code to run every time
+static inline void executeAssemblyReturn() {
+	((void (*)()) EXECUTE_ASSEMBLY_RETURN_ADDRESS)();
+}
